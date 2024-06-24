6aca8d2de457361d1ee70a058407509961f9c2c4
mikeleo03 <leonmichael463@gmail.com>
2024-06-24 09:38:01 +0700

    [Feat] Initiate springboot-maven project

---
+++ Week 03 - Jun 24-28/lecture_5/src/main/java/com/example/lecture_5/Lecture5Application.java
@@ -0,0 +1,13 @@
+package com.example.lecture_5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lecture5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lecture5Application.class, args);
+	}
+
+}
