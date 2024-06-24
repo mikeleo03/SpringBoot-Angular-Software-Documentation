5850bf97a7bb5bf82663fba19129b175133356a2
mikeleo03 <leonmichael463@gmail.com>
2024-06-24 12:23:52 +0700

    [Feat] Inititalize maven spring project

---
+++ Week 03/Lecture 05/Assignment 02/lecture_5_hw/src/test/java/com/example/lecture_5_hw/Lecture5HwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lecture_5_hw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture5HwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
