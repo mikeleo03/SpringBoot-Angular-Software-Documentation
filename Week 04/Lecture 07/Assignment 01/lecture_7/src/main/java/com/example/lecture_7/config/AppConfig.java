81579aa3efd53f15693ae20b41b8ce3beb205cbf
mikeleo03 <leonmichael463@gmail.com>
2024-07-01 15:32:32 +0700

    [Feat] Implementation of Assignment 01

---
+++ Week 04/Lecture 07/Assignment 01/lecture_7/src/main/java/com/example/lecture_7/config/AppConfig.java
@@ -0,0 +1,34 @@
+package com.example.lecture_7.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.lecture_7.EmployeeWork;
+import com.example.lecture_7.entity.Employee;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    public EmployeeWork employeeWork() {
+        return new EmployeeWork();
+    }
+
+    // Constructor Injection
+    // uncomment this and comment the Setter Injection implementation to demo
+    /* @Bean
+    public Employee employee() {
+        return new Employee("101", "John Doe", 30, employeeWork());
+    } */
+
+    // Setter Injection
+    @Bean
+    public Employee employee() {
+        Employee employee = new Employee();
+        employee.setId("101");
+        employee.setName("John Doe");
+        employee.setAge(30);
+        employee.setEmployeeWork(employeeWork());
+        return employee;
+    }
+}
