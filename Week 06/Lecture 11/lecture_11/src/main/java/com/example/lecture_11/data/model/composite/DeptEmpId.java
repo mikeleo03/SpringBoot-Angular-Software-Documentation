0036d7b1cf9c003361fe96713b8a905148b39608
mikeleo03 <leonmichael463@gmail.com>
2024-07-17 11:01:54 +0700

    [Feat] Lombok all args constructor

--- Week 06/Lecture 11/lecture_11/src/main/java/com/example/lecture_11/data/model/composite/DeptEmpId.java
+++ Week 06/Lecture 11/lecture_11/src/main/java/com/example/lecture_11/data/model/composite/DeptEmpId.java
@@ -2,9 +2,11 @@ package com.example.lecture_11.data.model.composite;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class DeptEmpId implements Serializable {
     private Integer empNo;
     private String deptNo;
--- Week 06/Lecture 11/lecture_11/src/main/java/com/example/lecture_11/data/model/composite/TitleId.java
+++ Week 06/Lecture 11/lecture_11/src/main/java/com/example/lecture_11/data/model/composite/TitleId.java
@@ -3,9 +3,11 @@ package com.example.lecture_11.data.model.composite;
 import java.io.Serializable;
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class TitleId implements Serializable {
     private Integer empNo;
     private String title;
