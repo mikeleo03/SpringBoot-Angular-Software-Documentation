7516f4e6b581d0b0c8a6f8bd996ca377fe80f95e
mikeleo03 <leonmichael463@gmail.com>
2024-08-20 23:22:53 +0700

    [Feat] Implementation of Login page

--- Week 11/standalone-app/src/app/components/login/login.component.ts
+++ Week 11/standalone-app/src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth.service';
 import { User } from '../../models/user.model';
 
@@ -6,14 +8,16 @@ import { User } from '../../models/user.model';
   selector: 'app-login',
   standalone: true,  // Important! This marks the component as standalone.
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: [],
   providers: [AuthService], // Services can be provided directly in the component
-  imports: [] // Import necessary modules directly into the component
+  imports: [FormsModule, CommonModule] // Import necessary modules directly into the component
 })
 export class LoginComponent {
   user: User = { id: 0, username: '', password: '' };
 
   constructor(private authService: AuthService) {}
+
+  mainbg: string = 'background.jpg';
 
   login() {
     this.authService.login(this.user).subscribe(response => {
