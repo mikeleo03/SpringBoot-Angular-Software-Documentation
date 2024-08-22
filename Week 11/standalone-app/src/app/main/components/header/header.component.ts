9099fdea416a1e9b153421a3535e90334246c1ea
mikeleo03 <leonmichael463@gmail.com>
2024-08-22 17:40:18 +0700

    [Feat] Finishing navbar

--- Week 11/standalone-app/src/app/main/components/header/header.component.ts
+++ Week 11/standalone-app/src/app/main/components/header/header.component.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HlmAvatarImageDirective, HlmAvatarComponent, HlmAvatarFallbackDirective } from '@spartan-ng/ui-avatar-helm';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [
+    CommonModule, 
+    HlmAvatarImageDirective, HlmAvatarComponent, HlmAvatarFallbackDirective,
+    HlmButtonDirective
+  ], // Import necessary modules
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: []
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  username = '';
+  showLogout = false;
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.username = 'Michael Leon';
+  }
+
+  handleLogout() {
+    this.router.navigate(['/login']); // Redirect to home after logout
+  }
 }
