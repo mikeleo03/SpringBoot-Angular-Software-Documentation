dd3b074472ddd8d92a507b4fd54931a65de1d9f4
mikeleo03 <leonmichael463@gmail.com>
2024-08-22 15:15:38 +0700

    [Feat] Hide header and footer for login page

--- Week 11/standalone-app/src/app/main/components/main/app.component.ts
+++ Week 11/standalone-app/src/app/main/components/main/app.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    CommonModule,
     RouterOutlet,
     HeaderComponent,
     FooterComponent
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss'] // Changed to styleUrls for correct property
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  showNavAndFooter = true;
   title = 'angular-demo';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Hide nav and footer on the login page
+        this.showNavAndFooter = event.url !== '/login';
+      }
+    });
+  }
 }
