34f4700914bd3cd8c7432d39393eccb619f4ff55
mikeleo03 <leonmichael463@gmail.com>
2024-09-07 10:32:29 +0700

    [Feat] Major project restructuring

--- Week 13/angular_oauth2/src/app/app.routes.ts
+++ Week 13/angular_oauth2/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { RouterConfig } from './config/app.constants';
+import { AuthGuard } from './main/guards/auth-guard.service';
+import { CallbackComponent } from './pages/login/callback.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: RouterConfig.LOGIN.path,
+        loadChildren: () =>
+            import('./pages/login/login.route').then((m) => m.loginRoutes),
+        title: RouterConfig.LOGIN.title,
+        data: RouterConfig.LOGIN.data,
+    },
+    { path: 'callback', component: CallbackComponent },
+    {
+        path: '',
+        canActivate: [AuthGuard],
+        children: [
+            {
+                path: RouterConfig.HOME.path,
+                loadChildren: () =>
+                    import('./pages/home/home.route').then((m) => m.homeRoutes),
+                title: "Home Page",
+                data: { header: true },
+            }
+        ],
+    },
+];
